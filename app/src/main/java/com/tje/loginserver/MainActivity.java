6cafb778140a756986395f35538268c8b6a26b9a
cunjj86 <ljgcunjj@naver.com>
2019-05-12 14:07:25 +0900

    SharedPrefrence 예제  - 로그인 버튼이 눌리면 입력되어 있던 ID를 저장   - 앱이 켜질때마다 마지막으로 저장된 ID를 자동입력

--- app/src/main/java/com/tje/loginserver/MainActivity.java
+++ app/src/main/java/com/tje/loginserver/MainActivity.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.tje.loginserver.databinding.ActivityMainBinding;
+import com.tje.loginserver.utils.ConnectServer;
+import com.tje.loginserver.utils.ContextUtil;
+
+import org.json.JSONObject;
 
 public class MainActivity extends BaseActivity {
 
@@ -27,11 +31,27 @@ public class MainActivity extends BaseActivity {
             @Override
             public void onClick(View v) {
 
-//                1. 아이디와 비번을 쿼라고 입력했는지 받아오기
+//                1. 아이디와 비번을 뭐라고 입력했는지 받아오기
+
+                String inputId = act.loginIdEdt.getText().toString();
+                String inputPw = act.loginPwEdt.getText().toString();
+
+//                1.2 입력받은 ID를 SharedPreferences 에 저장
+                ContextUtil.setUserInputId(mContext, inputId);
+
 //                2. 받아온 아이디와 비번이 정말로 올바른 회원인지 검사
 //                아이디/비번이 모두 동일한 사람이 회원 명부에 있는지 검사
 
-//                test123 / test123
+                ConnectServer.postRequestSignIn(mContext, inputId, inputPw, new ConnectServer.JsonResponseHandler() {
+                    @Override
+                    public void onResponse(JSONObject json) {
+
+
+
+                    }
+                });
+
+//                test123 / Test!123
 //                iu0001 / Test!123
 //                testorder1 / testorder1
 //                gggg1111 / gggg1111
@@ -43,6 +63,10 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setValues() {
+
+        String savedUserId = ContextUtil.getUserInputId(mContext);
+
+        act.loginIdEdt.setText(savedUserId);
 
     }
 
