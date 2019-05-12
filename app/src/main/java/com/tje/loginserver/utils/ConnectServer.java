b604172dc631c9ac706542997131da5d9baf744e
cunjj86 <ljgcunjj@naver.com>
2019-05-12 15:24:10 +0900

    My Info API 호출 설정

--- app/src/main/java/com/tje/loginserver/utils/ConnectServer.java
+++ app/src/main/java/com/tje/loginserver/utils/ConnectServer.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.FormBody;
+import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
@@ -57,6 +58,45 @@ public class ConnectServer {
             @Override
             public void onResponse(Call call, Response response) throws IOException {
 
+
+
+            }
+        });
+
+    }
+
+    public static void getRequestMyInfo(Context context, String token,final JsonResponseHandler handler) {
+
+//        서버 - 클라이언트 (앱)
+
+        OkHttpClient client = new OkHttpClient();
+
+//        URL 설정 => 목적지 설정
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(BASE_URL+"/v2/me_info").newBuilder();
+
+//        * GET, DELETE 메소드는 필요 파라미터를 URL에 담아줘야함.
+//         이 담는 과정을 쉽게 하려고 urlBuilder를 사용
+
+//        실제로 서버에 접근하는 완성된 URL
+        String requestURL = urlBuilder.build().toString();
+
+//        완성된 URL로 접근하는 request를 생성.
+        Request request = new Request.Builder()
+                .header("X-Http-Token", token)
+                .url(requestURL) // post 등의 메소드를 안쓰면, 기본적으로 GET 방식
+                .build();
+
+//        만들어진 request를 실제로 서버에 요청.
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
                 String responseContent = response.body().string();
 
                 Log.d("서버 응답 내용", responseContent);
@@ -78,5 +118,4 @@ public class ConnectServer {
         });
 
     }
-
 }
