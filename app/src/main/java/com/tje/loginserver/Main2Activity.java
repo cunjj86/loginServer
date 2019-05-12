6d15fd619472fab94a96ab939b425c6de5e8a883
cunjj86 <ljgcunjj@naver.com>
2019-05-12 15:37:43 +0900

    내 정보 가져오기 동작 확인.

--- app/src/main/java/com/tje/loginserver/Main2Activity.java
+++ app/src/main/java/com/tje/loginserver/Main2Activity.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.tje.loginserver.databinding.ActivityMain2Binding;
+import com.tje.loginserver.utils.ConnectServer;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Main2Activity extends BaseActivity {
 
@@ -35,6 +39,36 @@ public class Main2Activity extends BaseActivity {
         Log.d("사용자토큰값", token);
 
 //        받아온 토큰을 가지고 /v2/me_info API 호출. 사용자 데이터 표시
+
+        ConnectServer.getRequestMyInfo(mContext, token, new ConnectServer.JsonResponseHandler() {
+            @Override
+            public void onResponse(JSONObject json) {
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        try {
+                            int code = json.getInt("code");
+
+                            if (code == 200) {
+//                                정상적으로 데이터 수신
+
+                                JSONObject data = json.getJSONObject("data");
+
+                            }
+                            else {
+//                                비정상 상태
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                });
+            }
+        });
 
     }
 
